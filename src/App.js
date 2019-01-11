8c85787b44cc8c656083a8612a048079f0125fe9
Ben <bdhsu@berkeley.edu>
2019-01-11 15:01:05 -0800

    initial ui and form bound

--- src/App.js
+++ src/App.js
@@ -1,28 +1,103 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'normalize.css';
 import './App.css';
+import PropTypes from 'prop-types';
+
+class LocationInput extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            location: '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        var value = event.target.value;
+
+        this.setState(_ => {
+            return {
+                location: value,
+            }
+        });
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.props.onSubmit(
+            this.state.location,
+        );
+    }
+    render() {
+        return (
+            <div className="loc-container">
+                <form className="getweather" onSubmit={this.handleSubmit} style={this.props.style}>
+                    <input
+                        id="location"
+                        placeholder="San Francisco, CA"
+                        type="text"
+                        className="locinput"
+                        autoComplete="off"
+                        value={this.state.location}
+                        onChange={this.handleChange}
+                    />
+                    <button
+                        className="btn"
+                        type="submit"
+                        disabled={!this.state.location}>
+                            Get Weather
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+// LocationInput.propTypes = {
+//     location: PropTypes.string.isRequired,
+// }
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: '',
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(location) {
+        console.log('location: ' + location);
+        // this.setState(_ => {
+        //
+        // });
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <div className="header-logo">
+                        <a href="#" >Forecast</a>
+                    </div>
+                    <LocationInput style={{flexDirection: "row"}} />
+                </header>
+                <div className="container">
+                    <h1 className="text-center">Enter a City and State</h1>
+                    <LocationInput
+                        style={{flexDirection: "column"}}
+                        onSubmit={this.handleSubmit}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
